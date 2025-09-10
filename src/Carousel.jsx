41479a234e70c80f89818ac788485b710f998dea
York Yu <n11453559@gmail.com>
2025-09-10 16:01:41 +1000

    feat: add automatic slide transition to carousel

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { shortList, longList, list } from './data'
 import { FaQuoteRight } from 'react-icons/fa'
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
@@ -18,6 +18,16 @@ const Carousel = () => {
       return result
     })
   }
+
+  useEffect(() => {
+    const sliderId = setInterval(() => {
+      nextSlide()
+    }, 2000)
+
+    return () => {
+      clearInterval(sliderId)
+    }
+  }, [currentPerson])
 
   return (
     <section className="slider-container">
